d67754b9702ca367cecee6d9ff487001811712ee
Alcantara-git <rafael.ferreira.alcantara@gmail.com>
2026-09-10 12:56:31 -0300

    Add files via upload

--- database.js
+++ database.js
@@ -1,12 +1,16 @@
 const { Pool } = require('pg');
 
-// Aqui é onde a mágica acontece. 
-// O link abaixo conecta o seu site ao banco de dados do Supabase.
 const connectionString = process.env.DATABASE_URL || 'postgresql://postgres:W/NbpV_c45B*izk@db.cggyjbdpztsnhtrroiru.supabase.co:5432/postgres';
 
 const pool = new Pool({
     connectionString: connectionString,
-    ssl: { rejectUnauthorized: false } 
+    ssl: { rejectUnauthorized: false }
+});
+
+// Teste de conexão imediato
+pool.query('SELECT NOW()', (err, res) => {
+    if (err) console.error("ERRO DE CONEXÃO COM BANCO:", err.message);
+    else console.log("BANCO CONECTADO COM SUCESSO.");
 });
 
 module.exports = pool;
